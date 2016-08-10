fcaabe56a3d6179f6205617df85cde91f4003484
vikramlance <vikram.lance@gmail.com>
2016-08-09 22:57:53 -0500

    Update Project_1.py

--- Week_1/Project_1.py
+++ Week_1/Project_1.py
@@ -15,34 +15,34 @@ EX_GRAPH0, EX_GRAPH1, EX_GRAPH2 - Define three constants whose values are dictio
 these linked diagrams: EX_GRAPH0, EX_GRAPH1, and EX_GRAPH2.
 """
 #directed graphs 
-EX_GRAPH0 = {0:[1,2],
-             1:[],
-             2:[]}
+EX_GRAPH0 = {0:set([1,2]),
+             1:set([]),
+             2:set([])}
 
 print EX_GRAPH0
 
 
-EX_GRAPH1={0:[1,4,5],
-           1:[2,6],
-           2:[3],
-           3:[0],
-           4:[1],
-           5:[2],
-           6:[]}
+EX_GRAPH1={0:set([1,4,5]),
+           1:set([2,6]),
+           2:set([3]),
+           3:set([0]),
+           4:set([1]),
+           5:set([2]),
+           6:set([])}
 
 print EX_GRAPH1
 
 
-EX_GRAPH2 = {0: [1,4,5], 
-          1: [2,6],
-          2: [3,7],
-          3: [7],
-          4: [1],
-          5: [2],
-          6: [],
-          7: [3],
-          8: [1,2],
-          9: [0,3,4,5,6,7]}
+EX_GRAPH2 = {0: set([1,4,5]), 
+          1: set([2,6]),
+          2: set([3,7]),
+          3: set([7]),
+          4: set([1]),
+          5: set([2]),
+          6: set([]),
+          7: set([3]),
+          8: set([1,2]),
+          9: set([0,3,4,5,6,7])}
 print EX_GRAPH2
 
 num_nodes= int(raw_input().strip())
@@ -51,4 +51,4 @@ for i in range():
 print n
 
 if n<=0:
-	result_graph=
+	result_graph={}
